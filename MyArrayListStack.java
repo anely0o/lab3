11882839d0352770dc6d728b6c4837fa61a22a85
anely0o <ramyun.oppar@gmail.com>
2023-05-02 08:14:41 +0600

    Update MyArrayListStack.java

--- MyArrayListStack.java
+++ MyArrayListStack.java
@@ -1,5 +1,9 @@
 import java.util.EmptyStackException;
 public class MyArrayListStack {
     private myArrayList<Integer> list;
+    public myArrayListStack(){
+        list = new myArrayList<>();
+    }
+
 
 }
