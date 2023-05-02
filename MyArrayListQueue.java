8b119787d2b044272a9ec4e537dc46b590f82ccb
anely0o <ramyun.oppar@gmail.com>
2023-05-02 08:30:37 +0600

    Update MyArrayListQueue.java

--- MyArrayListQueue.java
+++ MyArrayListQueue.java
@@ -3,6 +3,9 @@ import java.util.NoSuchElementException;
 
 public class MyArrayListQueue<T> {
     private ArrayList<T> list;
-    
+    public myArrayListQueue(){
+        list = new ArrayList<T>();
+    }
+
 
 }
